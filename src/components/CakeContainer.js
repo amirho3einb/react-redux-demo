f031b3099e9fd29f55c1f98b9b1da5d0a07ebd4a
Amirhossein <amirhossinbalagar@gmail.com>
2022-02-01 21:14:25 +0330

    add redux to react

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,10 @@
+const CakeContainer = () => {
+    return (  
+        <div>
+            <h2>Number of cakes - </h2>
+            <button>buy cake</button>
+        </div>
+    );
+}
+ 
+export default CakeContainer;
---
+++ src/redux/cake/cakeActions.js
@@ -0,0 +1,7 @@
+import { BUY_CAKE } from './cakeTypes';
+
+function buyCake(){
+    return{
+        type: BUY_CAKE,
+    }
+}
